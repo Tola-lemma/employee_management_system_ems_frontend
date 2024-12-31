006f66f6c10e74202aba6a2dc1da81f2550ca864
Tola-lemma <tolalemma@gmail.com>
2024-12-31 12:35:36 +0300

    feat: apiSlice is included

---
+++ src/Components/API/apiSlice.jsx
@@ -0,0 +1,21 @@
+// import { apiURl } from '../Constants/api';/
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+const apiURl = 'http://localhost:5000';
+const baseQuery = fetchBaseQuery({
+baseUrl:apiURl,
+// credentials:'include',
+prepareHeaders:(headers,{getState})=>{
+      return headers;
+}
+})
+
+const baseQueryWithReauth = async (args, api, extraOptions)=>{
+      let result = await baseQuery(args, api , extraOptions);
+      return result;
+};
+
+export const apiSlice = createApi({
+      baseQuery:baseQueryWithReauth,
+      tagTypes:['ems'],
+      endpoints :(builder) => ({}),
+});

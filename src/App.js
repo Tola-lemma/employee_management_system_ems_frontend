9ae087aeca5d9c10e6f3252f02aee2ffeb2727cc
Tola-lemma <tolalemma@gmail.com>
2025-01-04 10:21:49 +0300

    feat:protected route is included

--- src/App.js
+++ src/App.js
@@ -2,29 +2,31 @@ import { Route, Routes } from "react-router-dom";
 import { PageNotFound } from "./Components/Container/Admin/Pages/PageNotFound/PageNotFound";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./Components/Container/Admin/theme";
-import { UserProvider } from "./Components/Container/Admin/Pages/global/LoginContext";
+import { AuthProvider } from "./Components/Container/Admin/Pages/global/LoginContext";
 import { ErrorProvider } from "./Components/Container/Admin/ToastErrorPage/ErrorContext";
 import { AdminPage } from "./Components/Container/Admin/Pages/Routes/AdminPage";
-// import {ProtectedRoute} from "./ProtectedRoute";     
+import {ProtectedRoute} from "./ProtectedRoute";     
 import { LoginPage } from "./Components/Container/Admin/LoginPage/LoginPage";
+import { ChangePassword } from "./Components/Container/Admin/LoginPage/ChangePassword";
 export const App = () => {
   const [theme, colorMode] = useMode("light");
   return (
     <ColorModeContext.Provider value={colorMode}>
-      <UserProvider>
+      <AuthProvider>
       <ErrorProvider>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          {/* <Route element={<ProtectedRoute/>}> */}
+          <Route path="/change_password/:token" element={<ChangePassword />} />
+          <Route element={<ProtectedRoute/>}>
           <Route path="/home/*" element={<AdminPage />} />
-          {/* </Route> */}
+          </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </ThemeProvider>
       </ErrorProvider>
-      </UserProvider>
+      </AuthProvider>
     </ColorModeContext.Provider>
   );
 };

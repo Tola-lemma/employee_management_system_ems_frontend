a464fd113e52c71f87d4f76728fd9422d384a2d1
Tola-lemma <tolalemma@gmail.com>
2025-01-04 15:15:21 +0300

    bug:protected route on load issue solved

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,7 +1,13 @@
 import React, { useContext } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 import AuthContext from './Components/Container/Admin/Pages/global/LoginContext';
+import { LoadingPage } from './PageLoading';
 export const ProtectedRoute = () => {
-      const { auth } = useContext(AuthContext);
-      return auth && auth.token ? <Outlet /> : <Navigate to="/" replace/>;
-};
+      const { auth, loading } = useContext(AuthContext);
+    
+      if (loading) {
+        return <LoadingPage/>
+      }
+    
+      return auth && auth.token ? <Outlet /> : <Navigate to="/" replace />;
+    };

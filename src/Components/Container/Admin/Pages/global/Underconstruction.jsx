547fe676750f7a53a57a376eefa8c87938f91296
Tola-lemma <tolalemma@gmail.com>
2025-01-10 21:30:13 +0300

    typo:Custom underconstration page type error fixed

--- src/Components/Container/Admin/Pages/global/Underconstruction.jsx
+++ src/Components/Container/Admin/Pages/global/Underconstruction.jsx
@@ -7,7 +7,7 @@ export const Underconstruction = () => {
   return (
     <Box m={"20px"}>
       <button style={{marginLeft:"600px",fontSize:"20px",padding:"5px",borderRadius:"20px"}} className="btn btn-primary" onClick={()=>navigate("/home")}>Go back to Home</button>
-      <Typography variant="body2" sx={{fontSize:"20px",fontStyle:"italic"}} color="initial">Comig Soon!</Typography>
+      <Typography variant="body2" sx={{fontSize:"20px",fontStyle:"italic"}} color="initial">Coming Soon!</Typography>
       <img style={{width:"70%" ,height:"600px",marginLeft:"200px" }} src="https://www.emigration.gov.eg/DefaultEn/images/website%20under%20construction.jpg" alt="UnderConstruction"/>
     </Box>
   )
